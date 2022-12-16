a89abcd60a2b5fbb92a5de0861b993dcd0aa21cb
Muhammad Fasal <mfmbt786@gmail.com>
2022-12-16 15:41:20 +0530

    Added files :tada:

--- jobkart/settings.py
+++ jobkart/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'gf$#$mgt#$h%bapwv)y%yvtcdf_k4@loqa&+=8hzc-$y)x0gua'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*',
-]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -78,12 +76,7 @@ WSGI_APPLICATION = 'jobkart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
