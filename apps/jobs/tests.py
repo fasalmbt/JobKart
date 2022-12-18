4c718389ca3b24dcac105759450b6e2c55de9791
fasalmbt <>
2022-12-18 16:15:41 +0530

    Core,Job(adding,apply) tests done :tada:

--- apps/jobs/tests.py
+++ apps/jobs/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
-from .forms import AddJobForm
+from .forms import AddJobForm, ApplyForm
 
 class AddJobFormTestCase(TestCase):
   def test_form_valid(self):
-    # Submit a valid form
     form_data = {
       'title': 'Software Engineer',
       'company': 'Acme Inc.',
@@ -14,7 +13,6 @@ class AddJobFormTestCase(TestCase):
     self.assertTrue(form.is_valid())
 
   def test_form_invalid(self):
-    # Submit a form with missing data
     form_data = {
       'title': '',
       'company': 'Acme Inc.',
@@ -23,5 +21,21 @@ class AddJobFormTestCase(TestCase):
     }
     form = AddJobForm(data=form_data)
     self.assertFalse(form.is_valid())
-    # Check that the form contains the appropriate error message
-    self.assertEqual(form.errors['title'], ['This field is required.'])
+
+
+class ApplyJobFormTestCase(TestCase):
+  def test_apply_form_valid(self):
+    form_data = {
+      'content': 'This is a coverletter for no reason',
+      'experience': '1-3 years'
+    }
+    form = ApplyForm(data=form_data)
+    self.assertTrue(form.is_valid())
+
+  def test_apply_form_invalid(self):
+    form_data = {
+      'content': 'This is a coverletter for no reason',
+      'experience': ''
+    }
+    form = ApplyForm(data=form_data)
+    self.assertFalse(form.is_valid())
